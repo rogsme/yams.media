50b55851532588f60de7e66819a0f174e594c197
Roger Gonzalez <roger@rogs.me>
2023-01-11 08:59:42 -0300

    Removed unnecesary ignores in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.webp"' _ {} \;
-find . -type f -not -path '*/.git/*' \( ! -iname ".gitignore" ! -iname "pre-commit" \) -exec sed -i -e 's/\.png/\.webp/g' {} \;
+find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.png/\.webp/g' {} \;
 hugo -s . -d /var/www/yams.media/
