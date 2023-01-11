257aa2bfa80ba18ef57d449ec9d826525f467e6a
Roger Gonzalez <roger@rogs.me>
2023-01-11 08:54:43 -0300

    Fixed ./build script to generate webp files

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.png"' _ {} \;
-find . -type f -not -path '*/.git/*' \( ! -iname ".gitignore" ! -iname "pre-commit" \) -exec sed -i -e 's/\.png/\.png/g' {} \;
+find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.webp"' _ {} \;
+find . -type f -not -path '*/.git/*' \( ! -iname ".gitignore" ! -iname "pre-commit" \) -exec sed -i -e 's/\.png/\.webp/g' {} \;
 hugo -s . -d /var/www/yams.media/
