4f5ebf1816c3308709966bf9704884f59616b33e
Roger Gonzalez <roger@rogs.me>
2023-01-13 16:23:21 -0300

    Readded cleanup on build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.webp"' _ {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.png/\.webp/g' {} \;
+rm -rf /var/www/yams.media/*
 hugo -s . -d /var/www/yams.media/
