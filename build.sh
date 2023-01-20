a6f0f39384a5255b883a6146f69514b00a3c0bb1
Roger Gonzalez <roger@rogs.me>
2023-01-19 22:20:29 -0300

    Changed hugo build command

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.webp"' _ {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.png/\.webp/g' {} \;
-rm -rf /var/www/yams.media/*
-hugo -s . -d /var/www/yams.media/
+hugo -s . -d /var/www/yams.media/ --minify --cleanDestinationDir --gc --verbose
