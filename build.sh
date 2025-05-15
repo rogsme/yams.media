c47c2cda7562723247c8b510ed292ca76ae12494
Roger Gonzalez <roger@rogs.me>
2025-05-14 21:22:03 -0300

    Add hugo cache directory
    - Use a cache directory for hugo builds.
    - This improves build speed by caching hugo assets.
    - The cache directory is located in $PWD/hugo-cache.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.webp"' _ {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.png/\.webp/g' {} \;
-hugo -s . -d /var/www/fr.yams.media/ --minify --cleanDestinationDir --gc --verbose
+hugo -s . -d /var/www/fr.yams.media/ --minify --cleanDestinationDir --gc --verbose --cacheDir $PWD/hugo-cache
